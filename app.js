ef7499337424935fd25f4118a43dce4ce424e9dd
Lucien Lin <luciensday@gmail.com>
2023-08-30 19:43:12 +0100

    js - add close x function

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const body = 
+const closebtns = document.getElementsByClassName("close");
+for ( let i =0; i < closebtns.length; i++){
+    closebtns[i].addEventListener('click', function() {
+        this.parentElement.style.display = 'none'
+    }); 
+}
+
+
