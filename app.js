c3aaee23ed6426f678825037e941a08ec77cb9eb
Lucien Lin <luciensday@gmail.com>
2023-09-02 18:33:12 +0100

    css: set up color root

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+
+
 const closebtns = document.getElementsByClassName("close");
+
 for ( let i =0; i < closebtns.length; i++){
     closebtns[i].addEventListener('click', function() {
+
+        const items = document.getElementsByClassName("colour")
+
+        if (items.length > 1 ) {
         this.parentElement.style.display = 'none'
+        } else {
+        this.style.display = 'none'
+        }
+
+
     }); 
 }
+
 
 
